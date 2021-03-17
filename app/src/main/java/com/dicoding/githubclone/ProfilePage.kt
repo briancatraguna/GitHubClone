2d1a07fc000b71d2d56eefd1fcff1556ec2d08f2
briancatraguna <briancatraguna@rocketmail.com>
2021-03-18 00:35:42 +0700

    Toolbar progress

--- app/src/main/java/com/dicoding/githubclone/ProfilePage.kt
+++ app/src/main/java/com/dicoding/githubclone/ProfilePage.kt
@@ -2,10 +2,22 @@ package com.dicoding.githubclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
-class ProfilePage : AppCompatActivity() {
+class ProfilePage : AppCompatActivity(),View.OnClickListener {
+    private lateinit var moreButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
+
+        moreButton = findViewById(R.id.more_button)
+        moreButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(applicationContext,"App under construction", Toast.LENGTH_SHORT).show()
     }
 }
