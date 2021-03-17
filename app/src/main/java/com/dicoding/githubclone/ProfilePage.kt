4778939092090709276d1011223f411247aeaf55
briancatraguna <briancatraguna@rocketmail.com>
2021-03-18 01:19:56 +0700

    Back button in profile page good to go

--- app/src/main/java/com/dicoding/githubclone/ProfilePage.kt
+++ app/src/main/java/com/dicoding/githubclone/ProfilePage.kt
@@ -1,13 +1,16 @@
 package com.dicoding.githubclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.Toast
 
 class ProfilePage : AppCompatActivity(),View.OnClickListener {
     private lateinit var moreButton: ImageView
+    private lateinit var backToMain: RelativeLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +18,29 @@ class ProfilePage : AppCompatActivity(),View.OnClickListener {
 
         moreButton = findViewById(R.id.more_button)
         moreButton.setOnClickListener(this)
+
+        backToMain = findViewById(R.id.back_to_main)
+        backToMain.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        Toast.makeText(applicationContext,"App under construction", Toast.LENGTH_SHORT).show()
+        when (v?.id) {
+            R.id.more_button -> {
+                val mFragmentManager = supportFragmentManager
+                val optionDialogFragment = OptionDialogFragment()
+                val fragment =
+                    mFragmentManager.findFragmentByTag(OptionDialogFragment::class.java.simpleName)
+                if (fragment !is OptionDialogFragment) {
+                    mFragmentManager
+                        .beginTransaction()
+                        .add(R.id.frame_container, optionDialogFragment, OptionDialogFragment::class.java.simpleName)
+                        .commit()
+                }
+            }
+            R.id.back_to_main -> {
+                val backToMainIntent: Intent = Intent(this,MainActivity::class.java)
+                startActivity(backToMainIntent)
+            }
+        }
     }
 }
