e8fd41f038d968428d101b94aaf16e6e884c68ba
briancatraguna <briancatraguna@rocketmail.com>
2021-04-01 23:13:12 +0700

    networking worked and added implicit intent to github page

--- app/src/main/java/com/dicoding/githubclone/MainActivity.kt
+++ app/src/main/java/com/dicoding/githubclone/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dicoding.githubclone
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.githubclone.databinding.ActivityMainBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.TextHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 
 class MainActivity : AppCompatActivity(){
 
@@ -25,16 +31,34 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val backButton = binding.toolbar.backGroup
-        backButton.setOnClickListener{
-            Toast.makeText(applicationContext,"App under construction",Toast.LENGTH_SHORT).show()
-        }
-
         rvProfiles = binding.rvGithubProfiles
         rvProfiles.setHasFixedSize(true)
 
+        val gitHubToolbarButton = binding.toolbar.gitHubLogo
+        gitHubToolbarButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.github.com"))
+            startActivity(intent)
+        }
+
         list.addAll(ProfilesData.listData)
         showRecyclerList()
+
+        getGitHubData()
+    }
+
+    private fun getGitHubData() {
+        val asyncClient = AsyncHttpClient()
+        asyncClient.addHeader("Authorization","token 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")
+        asyncClient.addHeader("User-Agent","request")
+        asyncClient.get("https://api.github.com/users/briancatraguna",object :TextHttpResponseHandler(){
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseString: String?) {
+                Log.d(TAG,"Connection Successful...")
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
+                Log.d(TAG,"Connection Failed...")
+            }
+        })
     }
 
     private fun showRecyclerList(){
@@ -42,4 +66,5 @@ class MainActivity : AppCompatActivity(){
         val listProfileAdapter = ListProfileAdapter(list)
         rvProfiles.adapter = listProfileAdapter
     }
+
 }
