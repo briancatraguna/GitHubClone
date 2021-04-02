d6f245b0768ec0b0db55a2da4fcf7de29114f464
briancatraguna <briancatraguna@rocketmail.com>
2021-04-02 18:59:55 +0700

    CONNECTION WORKED!

--- app/src/main/java/com/dicoding/githubclone/SearchUsers.kt
+++ app/src/main/java/com/dicoding/githubclone/SearchUsers.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import com.dicoding.githubclone.databinding.ActivitySearchUsersBinding
 import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
 import com.loopj.android.http.TextHttpResponseHandler
 import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.lang.Exception
 
 class SearchUsers : AppCompatActivity() {
 
@@ -26,22 +29,37 @@ class SearchUsers : AppCompatActivity() {
             finish()
         }
 
-        
+        getDevelopersData()
 
     }
 
-    private fun getGitHubData() {
+    private fun getDevelopersData() {
         val asyncClient = AsyncHttpClient()
-        asyncClient.addHeader("Authorization","token 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")
+        asyncClient.addHeader("Authorization","token  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")
         asyncClient.addHeader("User-Agent","request")
-        asyncClient.get("https://api.github.com/users/briancatraguna",object :
-            TextHttpResponseHandler(){
-            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseString: String?) {
-                Log.d(TAG,"Connection Successful...")
+        asyncClient.get("https://api.github.com/users/briancatraguna",object : TextHttpResponseHandler(){
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseString: String?
+            ) {
+                val result = responseString
+                try {
+                    val responseObject = JSONObject(result)
+                    val username = responseObject.getString("login")
+                    Log.d(TAG,"Connection Succesful.. Name: $username")
+                } catch (e:Exception){
+                    e.printStackTrace()
+                }
             }
 
-            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
-                Log.d(TAG,"Connection Failed...")
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseString: String?,
+                throwable: Throwable?
+            ) {
+                Log.d(TAG,"Connection Failed")
             }
         })
     }
