9040946a54e25d5d761299ba5b38a6b7481e99e3
briancatraguna <briancatraguna@rocketmail.com>
2021-04-05 13:32:57 +0700

    Adapter done

--- app/src/main/java/com/dicoding/githubclone/SearchProfileAdapter.kt
+++ app/src/main/java/com/dicoding/githubclone/SearchProfileAdapter.kt
@@ -1,10 +1,47 @@
 package com.dicoding.githubclone
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.dicoding.githubclone.data.model.User
+import com.dicoding.githubclone.databinding.ItemRowDeveloperProfilesBinding
 
 class SearchProfileAdapter: RecyclerView.Adapter<SearchProfileAdapter.UserViewHolder>() {
-    class UserViewHolder {
 
+    private val list = ArrayList<User>()
+
+    fun setList(users: ArrayList<User>){
+        list.clear()
+        list.addAll(users)
+        notifyDataSetChanged()
+    }
+
+    inner class UserViewHolder(val binding: ItemRowDeveloperProfilesBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User){
+            binding.apply {
+                Glide.with(itemView)
+                    .load(user.avatarUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .centerCrop()
+                    .into(imgProfilePictureSearch)
+                usernameSearch.text = user.login
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = ItemRowDeveloperProfilesBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
     }
 
 }
--- app/src/main/java/com/dicoding/githubclone/SearchUsers.kt
+++ app/src/main/java/com/dicoding/githubclone/SearchUsers.kt
@@ -3,20 +3,17 @@ package com.dicoding.githubclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.githubclone.databinding.ActivitySearchUsersBinding
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
-import com.loopj.android.http.TextHttpResponseHandler
-import cz.msebera.android.httpclient.Header
-import org.json.JSONArray
-import org.json.JSONObject
-import java.lang.Exception
 
 class SearchUsers : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchUsersBinding
-    private var list = ArrayList<String>()
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter : SearchProfileAdapter
 
     companion object {
         private val TAG = SearchUsers::class.java.simpleName
@@ -27,5 +24,29 @@ class SearchUsers : AppCompatActivity() {
         binding = ActivitySearchUsersBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        adapter = SearchProfileAdapter()
+        adapter.notifyDataSetChanged()
+        viewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
+        binding.apply {
+            rvUserSearch.layoutManager = LinearLayoutManager(this@SearchUsers)
+            rvUserSearch.setHasFixedSize(true)
+            rvUserSearch.adapter = adapter
+
+            btnSearch.setOnClickListener{
+
+            }
+            etQuery.setOnKeyListener { v, keyCode, event ->
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER){
+                    return@setOnKeyListener true
+                }
+                return@setOnKeyListener false
+            }
+        }
+
+    }
+
+    private fun showLoading(state: Boolean){
+        if (state)
     }
 }
